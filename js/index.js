63f6f8f3a8a33c21f9655e1e8bd579834b4bdd86
Rogerio Fernandes <grupocastanhal@hotmail.com>
2022-06-30 15:42:00 -0300

    Foi removido um comentario desnecessário

--- js/index.js
+++ js/index.js
@@ -5,13 +5,6 @@ let botao = document.getElementById('botao');
 // 3 - referenciar a lista
 let lista = document.getElementById("lista");
 
-/*                     
-<li class="list-group-item list-group-item-action">Jogar GTA</li>                  
-<li class="list-group-item list-group-item-action">Estudar python</li>            
-<li class="list-group-item list-group-item-action">Estudar javascript</li>           
-<li class="list-group-item list-group-item-action">Ver um filme</li>            
-<li class="list-group-item list-group-item-action">Ler um livro</li> */
-
 let tarefas = [
     'Jogar GTA 5',
     'Estudar Python',
